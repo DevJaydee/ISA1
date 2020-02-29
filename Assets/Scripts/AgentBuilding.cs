b992e62e160d7bf9afbb889fe95094da1bead837
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-02-29 15:21:15 +0100

    Add Regions, comments and functionality.
    Add some regions to clean up the script. Along with comments to clarify stuff. Add a Basic spawning script.

--- Assets/Scripts/AgentBuilding.cs
+++ Assets/Scripts/AgentBuilding.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class AgentBuilding : MonoBehaviour
 {
+	#region Variables
 	[SerializeField] private GameObject agentPrefab = default;  // Which agent to spawn form this building.
-	[SerializeField] private int maxAgents = 1;					// How many agents belong to this building.
+	[SerializeField] private int maxAgents = 1;                 // How many agents belong to this building.
+	[Space]
+	[SerializeField] private List<GameObject> activeAgents = new List<GameObject>();    // How many agents are active.
+	#endregion
+
+	#region Getters And Setters
+	public GameObject AgentPrefab { get => agentPrefab; set => agentPrefab = value; }
+	public int MaxAgents { get => maxAgents; set => maxAgents = value; }
+	public List<GameObject> ActiveAgents { get => activeAgents; set => activeAgents = value; }
+	#endregion
+
+	#region Monobehaviour Callbacks
+	private void Start()
+	{
+		SpawnAgents();
+	}
+	#endregion
+
+	#region Spawning Agents
+	private void SpawnAgents()
+	{
+		for(int i = 0; i < maxAgents; i++)
+		{
+			Instantiate(agentPrefab, transform.position, Quaternion.identity);
+		}
+	}
+	#endregion
 }
