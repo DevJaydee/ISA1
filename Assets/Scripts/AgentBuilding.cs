f28f34ff9ee54840c9ffc7f212210aeeefb4a293
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-02-29 15:12:12 +0100

    Add variable
    This one is purely done to test how branching works

--- Assets/Scripts/AgentBuilding.cs
+++ Assets/Scripts/AgentBuilding.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class AgentBuilding : MonoBehaviour
 {
-	[SerializeField] private GameObject agentPrefab = default;	// Which agent to spawn form this building.
+	[SerializeField] private GameObject agentPrefab = default;  // Which agent to spawn form this building.
+	[SerializeField] private int maxAgents = 1;					// How many agents belong to this building.
 }
