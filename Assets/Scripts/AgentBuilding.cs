f0255f2355f452aa8ead39fe5951cb432b4537fc
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-02-29 15:08:35 +0100

    Add AgentBuilding.cs

---
+++ Assets/Scripts/AgentBuilding.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBuilding : MonoBehaviour
+{
+	[SerializeField] private GameObject agentPrefab = default;	// Which agent to spawn form this building.
+}
