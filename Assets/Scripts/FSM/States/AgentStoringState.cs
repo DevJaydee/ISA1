8fa47bafcbffbbaa5db98deaf4b24770db7f74c5
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-16 17:08:37 +0100

    Change Functionality
    Change the functionality to work together with the AgentState and the new and improved states

---
+++ Assets/Scripts/FSM/States/AgentStoringState.cs
@@ -0,0 +1,45 @@
+﻿using RayWenderlich.Unity.StatePatternInUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentStoringState : State
+{
+	private float interactionInterval;
+
+	public AgentStoringState(Agent agent, StateMachine stateMachine) : base(agent, stateMachine)
+	{
+	}
+
+	public override void Enter()
+	{
+		base.Enter();
+
+		Debug.Log("Starting to Interact!");
+		interactionInterval = agent.InteractionInterval;
+	}
+
+	public override void Exit()
+	{
+		base.Exit();
+	}
+
+	public override void LogicUpdate()
+	{
+		base.LogicUpdate();
+		if(!agent.Target || agent.ResourceInventory <= 0)
+		{
+			agent.Target = null;
+			agent.AgentState = AgentState.Collecting;
+			stateMachine.ChangeState(agent.SearchingState);
+		}
+
+		interactionInterval -= Time.deltaTime;
+
+		if(interactionInterval <= 0)
+		{
+			agent.Target.GetComponent<IInteractable>()?.Interact(agent, agent.GatherAmount);
+			interactionInterval = agent.InteractionInterval;
+		}
+	}
+}
