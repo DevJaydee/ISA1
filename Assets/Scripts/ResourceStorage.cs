0c968ccaa875d6611863694ab6e80538a28f0f9e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-22 13:37:42 +0100

    Implement new storage system through scriptableobjects

--- Assets/Scripts/ResourceStorage.cs
+++ Assets/Scripts/ResourceStorage.cs
@@ -6,55 +6,48 @@ public class ResourceStorage : MonoBehaviour, IInteractable
 {
 	#region Variables
 	[SerializeField] private int maxItemsToBeStored = 100;  // How many items can be stored. (This can be of any type)
-	[SerializeField] private Dictionary<string, int> itemsStored = new Dictionary<string, int>();   // A dictionary with all the resources stored.
-	[SerializeField] private List<Item> itemsStoredSerialized = new List<Item>();   // List with all the stored items but serialized.
+	[SerializeField] private List<ResourceItem> itemsStored = new List<ResourceItem>();   // List with all the stored items but serialized.
 	#endregion
 
 	#region Getters & Setters
 
 	#endregion
 
+	private void OnApplicationQuit()
+	{
+		CleanupStorage();
+	}
+
 	public void Interact(Agent agent, int amount)
 	{
-		if(itemsStored.ContainsKey(agent.CollectedResourceName))
+		for(int i = 0; i < itemsStored.Count; i++)
 		{
-			itemsStored[agent.CollectedResourceName] += amount;
-			for(int i = 0; i < itemsStoredSerialized.Count; i++)
-				if(itemsStoredSerialized[i].name == agent.CollectedResourceName)
-					itemsStoredSerialized[i].Amount += amount;
-		}
-		else
-		{
-			itemsStored.Add(agent.CollectedResourceName, amount);
-
-			Item newItem = new Item
+			if(itemsStored[i].Name == agent.CollectedResourceName)
 			{
-				name = agent.CollectedResourceName,
-				Amount = amount
-			};
-			itemsStoredSerialized.Add(newItem);
-		}
+				int storedAmount = itemsStored[i].AmountStored;
+				storedAmount += amount;
+				itemsStored[i].AmountStored = storedAmount;
 
-		agent.ResourceInventory -= amount;
+				agent.ResourceInventory -= amount;
+			}
+		}
 	}
 
 	public bool HasSpace()
 	{
 		int totalItemsStored = 0;
+
 		for(int i = 0; i < itemsStored.Count; i++)
-		{
-		}
+			totalItemsStored += itemsStored[i].AmountStored;
 
 		return totalItemsStored <= maxItemsToBeStored ? true : false;
 	}
-}
 
-[System.Serializable]
-public class Item : ResourceStorage
-{
-	[SerializeField] private new string name = default;   // Name of the Item.
-	[SerializeField] private int amount = default;    // Amount of the Item.
-
-	public string Name { get => name; set => name = value; }
-	public int Amount { get => amount; set => amount = value; }
+	private void CleanupStorage()
+	{
+		foreach(ResourceItem item in itemsStored)
+		{
+			item.AmountStored = 0;
+		}
+	}
 }
