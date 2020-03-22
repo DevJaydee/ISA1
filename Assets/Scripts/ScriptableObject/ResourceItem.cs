e215c68802623fceaa508d8369b509bb2ad1265d
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-03-22 13:37:02 +0100

    Add ResourceItem ScriptableObject

---
+++ Assets/Scripts/ScriptableObject/ResourceItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResourceItem", menuName = "ScriptableObjects/New ResourceItem")]
+public class ResourceItem : ScriptableObject
+{
+	[SerializeField] private new string name = "";  // Name of the resource item.
+	[SerializeField] private int amountStored = 0;  // The amount of the resource that is stored.
+
+	public string Name { get => name; set => name = value; }
+	public int AmountStored { get => amountStored; set => amountStored = value; }
+}
