2843428f66da1e343732d013f6342e197c1f8954
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-10 18:09:59 +0200

    Add MainMenu.cs
    This class handles the Main Menu.

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] private GameObject MainMenuGameObject = default;       // Reference to the Main Menu GameObject.
+	[SerializeField] private GameObject SettingsMenuGameObject = default;   // Reference to the Settings Menu Object.
+	[SerializeField] private GameObject CreditsMenuGameObject = default;    // Reference to the Settings Menu Object.
+
+	/// <summary>
+	/// Starts the main game scene.
+	/// </summary>
+	public void StartGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	/// <summary>
+	/// Quits the application.
+	/// </summary>
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	/// <summary>
+	/// Opens the Settings Menu.
+	/// </summary>
+	public void ToggleSettingsMenu()
+	{
+		MainMenuGameObject.SetActive(!MainMenuGameObject.activeInHierarchy);
+		SettingsMenuGameObject.SetActive(!SettingsMenuGameObject.activeInHierarchy);
+	}
+
+	/// <summary>
+	/// Opens the Credits Menu.
+	/// </summary>
+	public void ToggleCreditsMenu()
+	{
+		MainMenuGameObject.SetActive(!MainMenuGameObject.activeInHierarchy);
+		CreditsMenuGameObject.SetActive(!CreditsMenuGameObject.activeInHierarchy);
+	}
+}
