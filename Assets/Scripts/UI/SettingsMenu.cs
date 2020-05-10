62d8c47813321eccd2667ec2a266e7b5f823df2e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-10 18:10:18 +0200

    Add SettingsMenu.cs
    This handles all the settings. (of which there are only 2)

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+	[SerializeField] private Slider cameraSensitivitySlider = default;      // Reference to the Camera Sensitivity Slider.
+	[SerializeField] private TextMeshProUGUI cameraSensitivitySliderValueTextMesh = default;    // Reference to the Camera Sensitivity Slider Value Text Mesh.
+	[Space]
+	[SerializeField] private Slider audioVolumeSlider = default;      // Reference to the Audio Volume Slider.
+	[SerializeField] private TextMeshProUGUI audioVolumeSliderValueTextMesh = default;    // Reference to the Audio Volume Slider Value Text Mesh.
+
+	private void Start()
+	{
+		cameraSensitivitySlider.value = PlayerPrefs.GetFloat("CameraSensitivity");
+		audioVolumeSlider.value = PlayerPrefs.GetFloat("AudioVolume");
+	}
+
+	public void UpdateCamSensitivity()
+	{
+		PlayerPrefs.SetFloat("CameraSensitivity", cameraSensitivitySlider.value);
+		cameraSensitivitySliderValueTextMesh.text = "Sensitivity: " + cameraSensitivitySlider.value.ToString("F2");
+	}
+
+	public void UpdateAudioVolume()
+	{
+		PlayerPrefs.SetFloat("AudioVolume", audioVolumeSlider.value);
+		audioVolumeSliderValueTextMesh.text = "Volume: " + audioVolumeSlider.value.ToString("F0");
+	}
+}
